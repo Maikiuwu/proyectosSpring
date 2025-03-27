4bdbffb1c65652b4f16d3f9c71ff51535af2490f
mega <sfssanchez@gmail.com>
2025-03-26 20:50:54 -0500

    todo arreglado

--- proyecto_2/src/main/java/com/example/proyecto_2/Model/Dao/IClienteDao.java
+++ proyecto_2/src/main/java/com/example/proyecto_2/Model/Dao/IClienteDao.java
@@ -1,11 +1,14 @@
 package com.example.proyecto_2.Model.Dao;
 
-import java.util.List;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.proyecto_2.Model.Entities.Cliente;
 
-public interface IClienteDao {
+@Repository
+public interface IClienteDao extends CrudRepository<Cliente, Long> {
     
-    public List<Cliente> findAll();
+    Optional<Cliente> findByNombre(String nombre);  
 
 }
